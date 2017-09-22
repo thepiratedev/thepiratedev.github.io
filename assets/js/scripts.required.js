09f160270bbc0fe2c48a019c25429379e3f0e5dd
peicap <peiwebdev@gmail.com>
2017-09-22 10:52:54 +0800

    Added scripts for materialize.

---
+++ assets/js/scripts.required.js
@@ -0,0 +1,6 @@
+$('.button-collapse').sideNav({
+  menuWidth: 300, // Default is 300
+  closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  draggable: true, // Choose whether you can drag to open on touch screens,
+  }
+);
